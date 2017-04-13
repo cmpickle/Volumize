99825a5b456fd3bfe975386f841e86a38cfa3ad3
Cameron Pickle <cmpickle@gmail.com>
2017-04-13 09:51:08 -0600

    misc changes

---
+++ app/src/main/java/com/cmpickle/volumize/domain/VolumeService.java
@@ -0,0 +1,11 @@
+package com.cmpickle.volumize.domain;
+
+import android.content.IntentFilter;
+
+/**
+ * @author Cameron Pickle
+ *         Copyright (C) Cameron Pickle (cmpickle) on 4/13/2017.
+ */
+
+public class VolumeService extends IntentFilter {
+}
