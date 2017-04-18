2134719efadd3d6b60a30efcb526e8d71658d78b
Cameron Pickle <cmpickle@gmail.com>
2017-04-17 22:36:45 -0600

    added child activity support, converted recognitions page to child activity

--- app/src/main/java/com/cmpickle/volumize/view/about/recognitions/RecognitionsActivity.java
+++ app/src/main/java/com/cmpickle/volumize/view/about/recognitions/RecognitionsActivity.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import com.cmpickle.volumize.R;
 import com.cmpickle.volumize.view.BaseActivity;
+import com.cmpickle.volumize.view.ChildActivity;
 
 /**
  * @author Cameron Pickle
  *         Copyright (C) Cameron Pickle (cmpickle) on 4/17/2017.
  */
 
-public class RecognitionsActivity extends BaseActivity {
+public class RecognitionsActivity extends ChildActivity {
     @Override
     protected Fragment createFragment() {
         return new RecognitionsFragment();
@@ -20,5 +22,10 @@ public class RecognitionsActivity extends BaseActivity {
     public static void start(Activity activity) {
         Intent intent = new Intent(activity, RecognitionsActivity.class);
         activity.startActivity(intent);
+    }
+
+    @Override
+    protected int getToolbarTitleId() {
+        return R.string.recognitions;
     }
 }
